acd7ac6aeca63e07d12e8fd621ced877affb48a8
Elizabeth Vitale <elizabethavitale@gmail.com>
2021-02-23 20:32:56 -0500

    added changes for javascript

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-
+function validate (){
+	var un = document.getElementById("un").value;
+	var pass = document.getElementById("pass").value;
+	
+	if(un == "" || pass == ""){
+		document.getElementById("error").innerHTML = "Username and password must both be filled out";
+	}else if(un.length < 6 || pass.length < 6){
+		document.getElementById("error").innerHTML = "Username and password must both be over 6 characters";
+	}else{
+		window.location = "login.html";
+	}
+}
 
 
 
